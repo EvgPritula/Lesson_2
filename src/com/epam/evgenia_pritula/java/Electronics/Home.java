a86fe839979cdb1b82fc008162f86779b8c223fa
Evgenia_Pritula <Evgenia_Pritula@epam.com>
2019-04-30 15:52:26 +0300

    Add

--- src/com/epam/evgenia_pritula/java/Electronics/Home.java
+++ src/com/epam/evgenia_pritula/java/Electronics/Home.java
@@ -1,4 +1,13 @@
 package com.epam.evgenia_pritula.java.Electronics;
 
 public class Home {
+private Appliances[] appliances;
+
+public Home(Appliances[] appliances){
+    this.appliances = appliances;
+}
+public Appliances[] getAppliances(){
+    return appliances;
+}
+
 }
